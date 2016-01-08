e5718e93166f4b65bf5b4d08e2edfc2d16573c4b
Ben Gray <gray.ben.m@gmail.com>
2016-01-08 16:03:01 +1100

    Mve sources here from other project

---
+++ src/main/java/generator/SeedBasedInputExpectedOutputGenerator.java
@@ -0,0 +1,31 @@
+package generator;
+
+/**
+ * Created by beng on 7/01/2016.
+ */
+public abstract class SeedBasedInputExpectedOutputGenerator<S, I, O>
+        implements InputAndExpectedOutputRetrievable<I, O> {
+
+    I input;
+    O expectedOutput;
+
+    protected SeedBasedInputExpectedOutputGenerator(S seed){
+        validateInitParams(seed);
+        this.input = generateInput(seed);
+        this.expectedOutput = generateExpectedOutput(seed);
+    }
+
+    protected abstract I generateInput(S seed);
+
+    protected abstract O generateExpectedOutput(S seed);
+
+    protected abstract void validateInitParams(S seed);
+
+    public I getInput() {
+        return this.input;
+    }
+
+    public O getExpectedOutput() {
+        return this.expectedOutput;
+    }
+}
