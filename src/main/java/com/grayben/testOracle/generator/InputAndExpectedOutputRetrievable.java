06c505d33475094f2d51a3b5035dd8ca14759612
Ben Gray <gray.ben.m@gmail.com>
2016-01-13 15:31:01 +1100

    Refactor interface into extension of two subinterfaces

--- src/main/java/com/grayben/testOracle/generator/InputAndExpectedOutputRetrievable.java
+++ src/main/java/com/grayben/testOracle/generator/InputAndExpectedOutputRetrievable.java
@@ -1,11 +1,11 @@
 package com.grayben.testOracle.generator;
 
+import com.grayben.testOracle.generator.input.InputRetrievable;
+import com.grayben.testOracle.generator.output.ExpectedOutputRetrievable;
+
 /**
  * Created by beng on 7/01/2016.
  */
-public interface InputAndExpectedOutputRetrievable<I, O> {
-
-    I getInput();
-
-    O getExpectedOutput();
+public interface InputAndExpectedOutputRetrievable<I, O>
+        extends InputRetrievable<I>, ExpectedOutputRetrievable<O> {
 }
---
+++ src/main/java/com/grayben/testOracle/generator/output/ExpectedOutputRetrievable.java
@@ -0,0 +1,8 @@
+package com.grayben.testOracle.generator.output;
+
+/**
+ * Created by beng on 13/01/2016.
+ */
+public interface ExpectedOutputRetrievable<O> {
+    O getExpectedOutput();
+}
