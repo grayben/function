9fd2f642b681c60fe09277b1554191e9fbba7212
Ben Gray <gray.ben.m@gmail.com>
2016-01-20 13:38:20 +1100

    Remove tuple (use apache commons instead), add parametrics

--- src/main/java/com/grayben/tools/math/parametricEquation/parameter/ParametricEquation.java
+++
@@ -1,8 +0,0 @@
-package com.grayben.tools.math.parametricEquation.parameter;
-
-/**
- * Created by beng on 19/01/2016.
- */
-public interface ParametricEquation<P> {
-    Tuple compute(P parameter)
-}
