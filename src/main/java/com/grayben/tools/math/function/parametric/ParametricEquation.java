f0fa742f22e41f71f4b09c8aa9344bd6653ba8c4
Ben Gray <gray.ben.m@gmail.com>
2016-02-01 17:45:47 +1100

    Use ImmutablePair

--- src/main/java/com/grayben/tools/math/function/parametric/ParametricEquation.java
+++ src/main/java/com/grayben/tools/math/function/parametric/ParametricEquation.java
@@ -1,12 +1,12 @@
 package com.grayben.tools.math.function.parametric;
 
-import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.function.Function;
 
 /**
  * Created by beng on 19/01/2016.
  */
-public interface ParametricEquation<T, X, Y> extends Function<T, Pair<X, Y>> {
+public interface ParametricEquation<T, X, Y> extends Function<T, ImmutablePair<X, Y>> {
 }
 
