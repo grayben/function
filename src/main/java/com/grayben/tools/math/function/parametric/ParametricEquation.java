ca61911291326893280422835978c0e8b82e59a5
Ben Gray <gray.ben.m@gmail.com>
2016-02-01 18:13:24 +1100

    Document ParametricEquation

--- src/main/java/com/grayben/tools/math/function/parametric/ParametricEquation.java
+++ src/main/java/com/grayben/tools/math/function/parametric/ParametricEquation.java
@@ -5,7 +5,12 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import java.util.function.Function;
 
 /**
- * Created by beng on 19/01/2016.
+ * Defines a {@link Function} which returns two values represented as an {@link ImmutablePair}.
+ * <p>
+ * Created by Ben Gray on 19/01/2016.
+ * @param <T> the independent variable type
+ * @param <X> the first dependent variable type
+ * @param <Y> the second dependent variable type
  */
 public interface ParametricEquation<T, X, Y> extends Function<T, ImmutablePair<X, Y>> {
 }
