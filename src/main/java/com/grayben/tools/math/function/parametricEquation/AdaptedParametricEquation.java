fbb1badcb366f71c511954260c92f4faa80d09b1
Ben Gray <gray.ben.m@gmail.com>
2016-02-01 17:32:58 +1100

    Rename type param P to T (as in T for time)

--- src/main/java/com/grayben/tools/math/function/parametricEquation/AdaptedParametricEquation.java
+++ src/main/java/com/grayben/tools/math/function/parametricEquation/AdaptedParametricEquation.java
@@ -4,18 +4,18 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.function.Function;
 
-public class AdaptedParametricEquation<P, A, X, Y> implements ParametricEquation<P, X, Y> {
+public class AdaptedParametricEquation<T, A, X, Y> implements ParametricEquation<T, X, Y> {
 
-    private final Function<P, A> inputAdapter;
+    private final Function<T, A> inputAdapter;
     private final ParametricEquation<A, X, Y> parametricEquation;
 
-    public AdaptedParametricEquation(Function<P, A> inputAdapter, ParametricEquation<A, X, Y> parametricEquation) {
+    public AdaptedParametricEquation(Function<T, A> inputAdapter, ParametricEquation<A, X, Y> parametricEquation) {
         this.inputAdapter = inputAdapter;
         this.parametricEquation = parametricEquation;
     }
 
     @Override
-    public Pair<X, Y> apply(P option) {
+    public Pair<X, Y> apply(T option) {
         return parametricEquation.compose(inputAdapter).apply(option);
     }
 }
