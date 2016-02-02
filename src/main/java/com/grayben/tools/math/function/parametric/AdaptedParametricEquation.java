07578db088e3b5d348d4136d9e74459e44734e5a
Ben Gray <gray.ben.m@gmail.com>
2016-02-02 11:58:33 +1100

    Document AdaptedParametricEquation

--- src/main/java/com/grayben/tools/math/function/parametric/AdaptedParametricEquation.java
+++ src/main/java/com/grayben/tools/math/function/parametric/AdaptedParametricEquation.java
@@ -4,11 +4,37 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.util.function.Function;
 
+/**
+ * A parametric equation which first adapts the input given before
+ * applying the underlying parametric equation to the adapted input.
+ * <p>
+ * This pattern can be chained by assigning another {@link AdaptedParametricEquation}
+ * to {@link #parametricEquation}.
+ * <p>
+ * Created by Ben Gray on 20/1/2016
+ * @param <T> the independent variable (parameter) type before adaptation
+ * @param <U> the independent variable (parameter) type after adaptation
+ * @param <X> the first dependent variable
+ * @param <Y> the second dependent variable
+ */
 public class AdaptedParametricEquation<T, U, X, Y> implements ParametricEquation<T, X, Y> {
 
+    /**
+     * The function which converts input of type <T>
+     */
     private final Function<T, U> inputAdapter;
+
+    /**
+     * The parametric equation which accepts the adapted input given by {@link #inputAdapter}
+     */
     private final ParametricEquation<U, X, Y> parametricEquation;
 
+    /**
+     * Constructs an adapted parametric equation using the specified
+     * input adapter and the specified underlying parametric equation.
+     * @param inputAdapter the input adapter
+     * @param parametricEquation the underlying parametric equation
+     */
     public AdaptedParametricEquation(Function<T, U> inputAdapter, ParametricEquation<U, X, Y> parametricEquation) {
         this.inputAdapter = inputAdapter;
         this.parametricEquation = parametricEquation;
