8a2ed8e45282edd9014712c84f06dd93f4515c65
Ben Gray <gray.ben.m@gmail.com>
2016-02-04 15:08:14 +1100

    Simplify the hell out of it and finish it (I think).

--- src/main/java/com/grayben/tools/math/function/parametric/FunctionBuilder.java
+++ src/main/java/com/grayben/tools/math/function/parametric/FunctionBuilder.java
@@ -1,82 +1,33 @@
 package com.grayben.tools.math.function.parametric;
 
-import java.lang.reflect.ParameterizedType;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
  * Created by Ben Gray on 3/02/2016.
  */
-public class FunctionBuilder<X, Y> {
+public class FunctionBuilder<U, V> implements Supplier<Function<U, V>> {
 
-    Class<?> intermediateType;
+    final private Function<U, V> function;
 
-    FunctionComponents<X, ?, Y> functionComponents;
-
-    public <A> void inputAdapter(Class<A> intermediateType, Function<X, A> inputAdapter){
-        this.intermediateType = intermediateType;
-        if (functionComponents == null) {
-            functionComponents = new FunctionComponents<>(intermediateType);
-        }
-        functionComponents = new FunctionComponents<>(intermediateType);
+    private FunctionBuilder(Function<U, V> underlyingFunction) {
+        this.function = underlyingFunction;
     }
 
-    public <A, B> void adaptInput(Class<B> newIntermediateType, Function<A, B> inputAdapter){
-        if (functionComponents.getClass().getTypeParameters().)
-        this.functionComponents =
+    public Function<U, V> build() {
+        return function;
     }
 
-    public Function<X, Y> build(){
-        return functionComponents.build();
+    public <A> FunctionBuilder<A, V> prependInputAdapter(Function<A, U> inputAdapter){
+        return new FunctionBuilder<>(inputAdapter.andThen(function));
     }
 
-    //TODO: use this construct in TestContainer
-    private static class FunctionComponents<X, A, Y> implements Supplier<Function<X, Y>> {
-
-        final private Class<A> intermediateType;
-
-        private Function<X, A> inputAdapter;
-        private Function<A, Y> function;
-
-        private FunctionComponents(Class<A> intermediateType) {
-            this.intermediateType = intermediateType;
-        }
-
-        public Function<X, Y> build() {
-            if (inputAdapter == null || function == null) {
-                throw new IllegalStateException("the inputAdapter and function have not both been set");
-            }
-            return inputAdapter.andThen(function);
-        }
-
-        public <B> FunctionComponents<X, B, Y> adaptInput(Function<A, B> inputAdapter){
-            FunctionComponents<X, B, Y> functionComponents = new FunctionComponents<>(intermediateType);
-            functionComponents.inputAdapter = this.inputAdapter.andThen(inputAdapter);
-            functionComponents.function = null;
-            return functionComponents;
-        }
-
-        public FunctionComponents<X, A, Y> function(Function<A, Y> function){
-            this.function = function;
-            return this;
-        }
-
-        @Override
-        public Function<X, Y> get() {
-            return this.build();
-        }
+    public <Z> FunctionBuilder<U, Z> appendOutputAdapter(Function<V, Z> outputAdapter){
+        return new FunctionBuilder<>(function.andThen(outputAdapter));
     }
 
-    public static void main(String[] args){
-        FunctionBuilder<String, String> functionBuilder = new FunctionBuilder<>();
-        functionBuilder.adaptInput(new Function<String, Integer>() {
-            @Override
-            public Integer apply(String s) {
-                return s.length();
-            }
-        });
-
+    @Override
+    public Function<U, V> get() {
+        return this.build();
     }
-
-
 }
